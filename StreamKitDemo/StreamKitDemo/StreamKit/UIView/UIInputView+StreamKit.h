21535a524f24dbaef968fc535e731d532af49481
godL <547188371@qq.com>
2017-01-19 17:13:33 +0800

    some issue

--- StreamKitDemo/StreamKitDemo/StreamKit/UIView/UIInputView+StreamKit.h
+++ StreamKitDemo/StreamKitDemo/StreamKit/UIView/UIInputView+StreamKit.h
@@ -10,8 +10,21 @@
 
 @interface UIInputView (StreamKit)
 
+/**
+ Creates a new inputView.
+ @code
+ UIInputView* input = UIInputView.sk_init(frame,inputViewStyle);
+ @endcode
+ @return a block which receive a frame and a inputViewStyle.
+ */
 + (UIInputView* (^)(CGRect frame,UIInputViewStyle inputViewStyle))sk_init;
 
+/**
+ Set allowsSelfSizing.
+ @code
+ self.sk_allowsSelfSizing(allowsSelfSizing);
+ @endcode
+ */
 - (UIInputView* (^)(BOOL allowsSelfSizing))sk_allowsSelfSizing;
 
 @end
