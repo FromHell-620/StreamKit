8db52311e7f3ef6facb7edbdd279a030507d4a28
Lsunan <hao.li@sunanzq.com>
2017-02-10 11:49:35 +0800

    update read

--- README.md
+++ README.md
@@ -24,7 +24,71 @@ Usage
 
 ### Basic 
     //(Create a UILabel)
-    UILabel* label = [UILabel new].
+    UILabel* label = [UILabel new];//UILabel.new
+    label.sk_frame(CGRectZero)
+    .sk_fontSize(15)
+    .sk_textAlignment(NSTextAlignmentCenter)
+    .sk_textColor([UIColor redColor])
+    .sk_text(@"a label")
+    .sk_addSimpleClickAction(^{
+        //click action
+    });
+    [self.view addSubview:label];
+
+
+    //event block(create a button)
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.sk_frame(CGRectMake(100, 100, 100, 100))
+    .sk_setTitleNormal(@"a button")
+    .sk_setTitleColorNormal([UIColor redColor])
+    .sk_setFontSize(15)
+    .sk_addEventBlock(UIControlEventTouchDown,^(UIButton* button){
+
+    })
+    .sk_addEventBlock(UIControlEventTouchUpInside,^(UIButton* button){
+
+    })
+    .sk_addEventBlock(UIControlEventTouchUpOutside,^(UIButton* button){
+
+    });
+    [self.view addSubview:button];
+
+
+
+    //delegate block(create a textField)
+    UITextField* textField = UITextField.sk_init(CGRectZero)
+    .sk_textColor([UIColor blackColor])
+    .sk_fontSize(15)
+    .sk_placeholder(@"begin")
+    .sk_addEventBlock(UIControlEventEditingChanged,^(UITextField* textField){
+
+    })
+    .sk_textFieldShouldReturn(^BOOL(UITextField* textField){
+        return YES;
+    })
+    .sk_textFieldShouldBeginEditing(^BOOL(UITextField* textField){
+        return YES;
+    })
+    .sk_textFieldShouldChangeCharactersInRange(^BOOL(UITextField* textField,NSRange range,NSString* string){
+        return YES;
+    });
+    [self.view addSubview:textField];
+
+
+    //KVO
+    label.sk_addObserverWithKeyPath(@"text",^(NSDictionary* change){
+
+    });
+
+
+    //NSNotification
+    NSNotificationCenter* defaultNotification = [NSNotificationCenter defaultCenter];
+    defaultNotification.sk_addNotification(UITextFieldTextDidChangeNotification,^(NSNotification* noti){
+
+    })
+    .sk_addNotification(UITextFieldTextDidEndEditingNotification,^(NSNotification* noti){
+
+    });
 
 License
 ==============
--- StreamKitDemo/StreamKitDemo/ViewController.m
+++ StreamKitDemo/StreamKitDemo/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "StreamContentController.h"
 #import <StreamKit/StreamKit.h>
 @interface ViewController ()
 
@@ -18,16 +17,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    /*
+     create a label
+     */
+    UILabel* label = [UILabel new];//UILabel.new
+    label.sk_frame(CGRectZero)
+    .sk_fontSize(15)
+    .sk_textAlignment(NSTextAlignmentCenter)
+    .sk_textColor([UIColor redColor])
+    .sk_text(@"a label")
+    .sk_addSimpleClickAction(^{
+    
+    });
+    [self.view addSubview:label];
+    
+    /*
+     event block
+     */
     UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
     button.sk_frame(CGRectMake(100, 100, 100, 100))
-    .sk_setTitleNormal(@"lihao").sk_setTitleColorNormal([UIColor redColor])
+    .sk_setTitleNormal(@"a button")
+    .sk_setTitleColorNormal([UIColor redColor])
     .sk_setFontSize(15)
     .sk_addEventBlock(UIControlEventTouchDown,^(UIButton* button){
         
-    }).sk_addEventBlock(UIControlEventTouchUpInside,^(UIButton* button){
+    })
+    .sk_addEventBlock(UIControlEventTouchUpInside,^(UIButton* button){
         
+    })
+    .sk_addEventBlock(UIControlEventTouchUpOutside,^(UIButton* button){
+    
     });
     [self.view addSubview:button];
+    
+    /*
+     delegate block
+     */
+    UITextField* textField = UITextField.sk_init(CGRectZero)
+                             .sk_textColor([UIColor blackColor])
+                             .sk_fontSize(15)
+                             .sk_placeholder(@"begin")
+                             .sk_addEventBlock(UIControlEventEditingChanged,^(UITextField* textField){
+    
+                              })
+                             .sk_textFieldShouldReturn(^BOOL(UITextField* textField){
+                                 return YES;
+                             })
+                             .sk_textFieldShouldBeginEditing(^BOOL(UITextField* textField){
+                                 return YES;
+                             })
+                             .sk_textFieldShouldChangeCharactersInRange(^BOOL(UITextField* textField,NSRange range,NSString* string){
+                                 return YES;
+                             });
+    [self.view addSubview:textField];
+    
+    /*
+     KVO
+     */
+    label.sk_addObserverWithKeyPath(@"text",^(NSDictionary* change){
+    
+    });
+    
+    /*
+     NSNotification
+     */
+    NSNotificationCenter* defaultNotification = [NSNotificationCenter defaultCenter];
+    defaultNotification.sk_addNotification(UITextFieldTextDidChangeNotification,^(NSNotification* noti){
+    
+    })
+    .sk_addNotification(UITextFieldTextDidEndEditingNotification,^(NSNotification* noti){
+    
+    });
     // Do any additional setup after loading the view, typically from a nib.
 }
 
